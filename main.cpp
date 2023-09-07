8c4020f1efe035e7c32faa4a674f94275cd6178b
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2023-09-07 18:17:39 -0300

    Insere default de projetos em c++

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "src/Funcao.h"
+
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+    funcao();
+
+    return 0;
+}
---
+++ src/Funcao.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "funcao.h"
+using namespace std;
+
+void funcao()
+{
+    cout << "Hello World 3" << endl;
+}
